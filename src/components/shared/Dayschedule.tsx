d494fd7b2f57d0bc28cffa0ddc991383a9924b62
Omkar-kamble82 <omkarsk82@gmail.com>
2024-09-10 20:06:00 +0530

    vercel.json and mobile responsive

--- src/components/shared/Dayschedule.tsx
+++ src/components/shared/Dayschedule.tsx
@@ -21,9 +21,9 @@ const Dayschedule = (props:Props) => {
         { 
             props.day.morning && (
             <>
-                <div className="text-yellow-800 font-bold text-xl ml-[15px] mt-[10px]">Morning</div>
+                <div className="text-yellow-800 font-bold text-xl sm:ml-[15px] mt-[10px]">Morning</div>
                 {props.day.morning.map(t => (
-                <div key={t.task} className="ml-[30px] mt-[5px] shadow-md">
+                <div key={t.task} className="sm:ml-[30px] mt-[5px] shadow-md">
                     <p className="bg-[#eee] rounded-xl py-3 px-1 text-secondary font-bold"><span className="text-yellow-600"><span className="bg-gray-700 p-2 rounded-xl text-yellow-500">{t.duration}</span> {t.task}:</span> {t.description}</p>
                 </div>
                 ))}
@@ -32,9 +32,9 @@ const Dayschedule = (props:Props) => {
         { 
             props.day.afternoon && (
             <>
-            <div className="text-yellow-800 font-bold text-xl ml-[15px] mt-[10px]">Afternoon</div>
+            <div className="text-yellow-800 font-bold text-xl sm:ml-[15px] mt-[10px]">Afternoon</div>
             {props.day.afternoon.map(t => (
-            <div key={t.task} className="ml-[30px] mt-[5px] shadow-md">
+            <div key={t.task} className="sm:ml-[30px] mt-[5px] shadow-md">
                 <p className="bg-[#eee] rounded-xl py-3 px-1 text-secondary font-bold"><span className="text-yellow-600"><span className="bg-gray-700 p-2 rounded-xl text-yellow-500">{t.duration}</span> {t.task}:</span> {t.description}</p>
             </div>
             ))}</>
